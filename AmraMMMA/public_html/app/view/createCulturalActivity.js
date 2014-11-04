bd5ee399bb911acb2d12478f72ef6ef854feb490
lalagaye@gmail.com <lalagaye@gmail.com@1f195644-4993-a81e-ac88-5f11828a4e59>
2014-11-04 18:55:17 +0000

    vue create changer, createsportactivity en cours pour la date mais ne marche pas trop pour l'instant

--- AmraMMMA/public_html/app/view/createCulturalActivity.js
+++ AmraMMMA/public_html/app/view/createCulturalActivity.js
@@ -6,10 +6,10 @@ Ext.define("Lan.view.createCulturalActivity", {
           iconCls: 'user',
           scrollable: 'vertical',
           items: [
-          {
+          /*{
             xtype: 'toolbar',
             title: 'Créer une activité'
-          },
+          },*/
           {
             xtype: 'textfield',
             name: 'id_user',
@@ -17,28 +17,43 @@ Ext.define("Lan.view.createCulturalActivity", {
          },
          {
             xtype: 'textfield',
+            name: 'title',
+            label: 'Titre'
+         },
+         {
+            xtype: 'textfield',
+            name: 'date_activity',
+            label: 'Date'
+        },
+         {
+            xtype: 'textfield',
             name: 'start_time',
-            label: 'Heure de debut de l\'activite'
+            label: 'Heure de début'
         },
         {
             xtype: 'textfield',
             name: 'stop_time',
-            label: 'Heure de fin de l\'activite'
+            label: 'Heure de fin'
         },
         {
             xtype: 'textfield',
             name: 'location',
+            label: 'Lieu'
+        },
+        {
+            xtype: 'textfield',
+            name: 'meeting_location',
             label: 'Lieu de rendez-vous'
         },
         {
             xtype: 'numberfield',
             name: 'max_number',
-            label: 'Nombre de personne maximal'
+            label: 'Nombre maximal de personnes'
         },
         {
-            xtype: 'textfield',
+            xtype: 'textareafield',
             name: 'description',
-            label: 'Decription de l\'activite'
+            label: 'Description de l\'activite'
         },
        
         
